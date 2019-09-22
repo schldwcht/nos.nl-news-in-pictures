463ee1a4edaa34c5b7eace15557ba67e132304fb
Joost Schildwacht <joost.schildwacht@tig.nl>
2019-09-22 11:17:23 +0200

    slug files

---
+++ Service/Slug.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Schldwcht\Newsinpictures\Service;
+
+class Slug
+{
+    /**
+     * @param string $text
+     *
+     * @return string
+     *
+     */
+    private function cleanString($text)
+    {
+        $utf8 = array(
+            '/[áàâãªä]/u' => 'a',
+            '/[ÁÀÂÃÄ]/u'  => 'A',
+            '/[ÍÌÎÏ]/u'   => 'I',
+            '/[íìîï]/u'   => 'i',
+            '/[éèêë]/u'   => 'e',
+            '/[ÉÈÊË]/u'   => 'E',
+            '/[óòôõºö]/u' => 'o',
+            '/[ÓÒÔÕÖ]/u'  => 'O',
+            '/[úùûü]/u'   => 'u',
+            '/[ÚÙÛÜ]/u'   => 'U',
+            '/ç/'         => 'c',
+            '/Ç/'         => 'C',
+            '/ñ/'         => 'n',
+            '/Ñ/'         => 'N',
+            '/–/'         => '-', // UTF-8 hyphen to "normal" hyphen
+            '/[’‘‹›‚]/u'  => ' ', // Literally a single quote
+            '/[“”«»„]/u'  => ' ', // Double quote
+            '/ /'         => ' ', // nonbreaking space (equiv. to 0x160)
+        );
+
+        return preg_replace(array_keys($utf8), array_values($utf8), $text);
+    }
+
+    /**
+     * @param $slug
+     *
+     * @return string
+     */
+    public function createSlug($slug)
+    {
+        $slug                        = $this->cleanString($slug);
+        $lettersNumbersSpacesHyphens = '/[^\-\s\pN\pL]+/u';
+        $spacesDuplicateHypens       = '/[\-\s]+/';
+
+        $slug = preg_replace($lettersNumbersSpacesHyphens, '', $slug);
+        $slug = preg_replace($spacesDuplicateHypens, '-', $slug);
+
+        $slug = trim($slug, '-');
+
+        return mb_strtolower($slug, 'UTF-8');
+    }
+
+}
